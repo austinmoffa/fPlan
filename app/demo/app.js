8caf4e5a219493328b5781c978096ac2e0fb7d65
Austin <austimoffa@gmail.com>
2016-03-02 10:17:53 -0500

    More useful demo code

--- app/demo/app.js
+++ app/demo/app.js
@@ -1,49 +1,17 @@
 angular.module('mainApp', ['FPlan'])
-.controller('MainController', function() {
+.controller('MainController', function($http) {
     var vm = this;
-    vm.maps = [
-        {
-            url:'./app/demo/images/floor1.svg',
-            title: 'Test Floor 1',
-            uid: 'map_1',
-        },
-        {
-            url:'./app/demo/images/floor2.svg',
-            title: 'Test Floor 2',
-            uid: 'map_2',
-        },
-    ];
 
-    vm.seats = {
-        map_1:[
-            {
-                uid: 's1',
-                coords: [20, 20],
-                name: 'Seat 1',
-            },
-        ]
-    };
-    vm.people = [
-        {
-            name: 'Ian Malcom',
-            uid: '123456789',
-            map_data: {
-                map_1: {
-                    seat: 's1',
-                    schedule: {
-                        monday: true,
-                        tuesday: true,
-                        wednesday: true,
-                        thursday: true,
-                        friday: true
-                    }
-                }
-            },
-            data: {
-                team: 'GIS',
-            }
+   $http.get('./app/demo/data/maps.json').then(function(res) {
+        vm.maps = res.data;
+   });
 
-        }
-    ]
+   $http.get('./app/demo/data/seats.json').then(function(res) {
+        vm.seats = res.data;
+   });
+
+   $http.get('./app/demo/data/people.json').then(function(res) {
+        vm.people = res.data;
+   });
 
 });
